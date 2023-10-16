031cf8d546655b7b9b67803bab623d82adce42c0
some-stupid-nickname <dmitrymironov.a@gmail.com>
2023-10-16 23:14:36 +0300

    переписал алгоритм

--- fleury.py
+++ fleury.py
@@ -1,63 +1,68 @@
 from collections import defaultdict
 
-
 class Graph:
+
     def __init__(self, vertices):
         self.V = vertices
         self.graph = defaultdict(list)
 
-    def add_edge(self, u, v):
+    def addEdge(self, u, v):
         self.graph[u].append(v)
         self.graph[v].append(u)
 
-    def is_bridge(self, u, v):
-        visited = [False] * self.V
-        count1 = self.dfs(u, visited)
-        self.graph[u].remove(v)
-        self.graph[v].remove(u)
-        visited = [False] * self.V
-        count2 = self.dfs(u, visited)
-        self.add_edge(u, v)
-   #     return count1 > count2
+    def removedg(self, u, v):
+        for index, key in enumerate(self.graph[u]):
+            if key == v:
+                self.graph[u].pop(index)
+        for index, key in enumerate(self.graph[v]):
+            if key == u:
+                self.graph[v].pop(index)
 
-    def dfs(self, v, visited):
+    def reachable(self, v, visited):
         count = 1
         visited[v] = True
         for i in self.graph[v]:
-            if not visited[i]:
-                count += self.dfs(i, visited)
-            return count
+            if visited[i] == False:
+                count = count + self.reachable(i, visited)
+        return count
 
-    def fleury_algorithm(self, start):
-        if self.has_eulerian_cycle():
-            path = []
-            self.fleury_util(start, path)
-            return '->'.join(map(str, path))
+
+    def isValidNextEdge(self, u, v):
+        if len(self.graph[u]) == 1:
+            return True
         else:
-            return "Граф не содержит эйлерового цикла."
+            visited = [False] * (self.V)
+            count1 = self.reachable(u, visited)
 
-    def fleury_util(self, u, path):
+            self.removedg(u, v)
+            visited = [False] * (self.V)
+            count2 = self.reachable(u, visited)
+
+            self.addEdge(u, v)
+
+            return False if count1 > count2 else True
+
+    def printEulerUtil(self, u):
         for v in self.graph[u]:
-            if not self.is_bridge(u, v):
-                path.append(u)
-                self.graph[u].remove(v)
-                self.graph[v].remove(u)
-            self.fleury_util(v, path)
+            if self.isValidNextEdge(u, v):
+                print("%d->%d " % (u, v))
+                self.removedg(u, v)
+                self.printEulerUtil(v)
 
-    def has_eulerian_cycle(self):
+    def printEulerTour(self):
+        u = 0
         for i in range(self.V):
             if len(self.graph[i]) % 2 != 0:
-                return False
-            return True
+                u = i
+                break
+        self.printEulerUtil(u)
 
-# Проверка:
 
-g = Graph(4)
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-g.add_edge(1, 2)
-g.add_edge(2, 3)
 
-start_vertex = 0
 
-print("Эйлеров путь: ", g.fleury_algorithm(start_vertex))
+g1 = Graph(4)
+g1.addEdge(0, 1)
+g1.addEdge(0, 2)
+g1.addEdge(1, 2)
+g1.addEdge(2, 3)
+g1.printEulerTour()
